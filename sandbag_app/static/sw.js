0a2e0b29dff8ed5379c43c12f02c6398c3f31a1d
ZuonVu <vuxuanduonng11@gmail.com>
2026-04-24 19:37:15 +0900

    Add SmartPunch to Home Screen

---
+++ sandbag_app/static/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (event) => {
+    console.log('Smart Sandbag App Installed');
+});
+
+self.addEventListener('fetch', (event) => {
+    // Allows the browser to fetch things normally
+});
